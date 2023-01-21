4a4cbe3a6a8341f736dc89bcb9a3ec6fb365adb2
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-01-21 14:11:37 +0530

    update

--- Blog+Chat+SendEth/Chat.sol
+++ Blog+Chat+SendEth/Chat.sol
@@ -5,8 +5,7 @@ pragma solidity >=0.8.0;
 contract Chat {
     uint256 public taskCount = 0;
     uint256 transactionCounter;
-    event Transfer(address from , address to , uint256 amount,  string message ,uint256 timestamp, string keyword);
-    
+
     struct TransferStruct{
         address sender;
         address receiver;
@@ -71,7 +70,8 @@ contract Chat {
     );
 
     event TaskCompleted(uint256 id, bool completed);
-
+    event Transfer(address from , address to , uint256 amount,  string message ,uint256 timestamp, string keyword);
+    
     function checkUserExists(address pubKey) public view returns (bool) {
         return bytes(userList[pubKey].name).length >0;
     }
