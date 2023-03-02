0dc97148e04ce59968b334fcf0317ced3939786f
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-03-02 18:12:02 +0530

    update add safemath

--- nftBasedErc20Lock.sol
+++ nftBasedErc20Lock.sol
@@ -1,11 +1,15 @@
 //SPDX-License-Identifier : MIT
+
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract DepositNFT is ERC721URIStorage, ReentrancyGuard, Ownable {
+    using SafeMath for uint256;
     IERC20 public depositToken;
     uint256 public lockDuration;
     uint256 public depositCap;
